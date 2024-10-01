642170ee16700973db76d1377534b57a4228c464
Simone Infante <simone.infante@pagopa.it>
2024-10-01 17:21:44 +0200

    init pro

---
+++ src/test/kotlin/it/pagopa/wallet/PagopaPaymentWalletCdcServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.pagopa.wallet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PagopaPaymentWalletCdcServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
