bdfef27e4ffb9cbaca322e1d07ac407c15e268ad
Simone Infante <simone.infante@pagopa.it>
2024-10-01 22:58:45 +0200

    chore: add spotless plugin

--- src/main/kotlin/it/pagopa/wallet/PagopaPaymentWalletCdcServiceApplication.kt
+++ src/main/kotlin/it/pagopa/wallet/PagopaPaymentWalletCdcServiceApplication.kt
@@ -3,9 +3,8 @@ package it.pagopa.wallet
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class PagopaPaymentWalletCdcServiceApplication
+@SpringBootApplication class PagopaPaymentWalletCdcServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<PagopaPaymentWalletCdcServiceApplication>(*args)
+    runApplication<PagopaPaymentWalletCdcServiceApplication>(*args)
 }
