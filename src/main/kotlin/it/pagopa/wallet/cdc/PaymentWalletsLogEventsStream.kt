7e4a9455540685d4606d96bf506ce1b19512cd33
CianoDanilo <danilo.ciano@pagopa.it>
2024-10-02 17:56:27 +0200

    feat(change-stream): add first implementation of change stream

---
+++ src/main/kotlin/it/pagopa/wallet/cdc/PaymentWalletsLogEventsStream.kt
@@ -0,0 +1,45 @@
+package it.pagopa.wallet.cdc
+
+import org.bson.BsonDocument
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.data.mongodb.core.ChangeStreamEvent
+import org.springframework.data.mongodb.core.ChangeStreamOptions
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+
+@Component
+class PaymentWalletsLogEventsStream(
+    @Autowired private val reactiveMongoTemplate: ReactiveMongoTemplate
+) : ApplicationListener<ApplicationReadyEvent> {
+    private val logger = LoggerFactory.getLogger(PaymentWalletsLogEventsStream::class.java)
+
+    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+        this.stream()
+    }
+
+    fun stream() {
+        val flux: Flux<ChangeStreamEvent<BsonDocument>> =
+            reactiveMongoTemplate.changeStream(
+                "payment-wallets-log-events",
+                ChangeStreamOptions.builder()
+                    .filter(
+                        Aggregation.newAggregation(
+                            Aggregation.match(
+                                Criteria.where("operationType").`in`("insert", "update", "replace")
+                            ),
+                            Aggregation.project("fullDocument")
+                        )
+                    )
+                    .build(),
+                BsonDocument::class.java
+            )
+
+        flux.subscribe { it -> logger.info(it.toString()) }
+    }
+}
