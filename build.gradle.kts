ac323280e6366a28b0b9bdb447af80244b248c04
CianoDanilo <danilo.ciano@pagopa.it>
2024-10-03 17:50:21 +0200

    wip(change-stream): add first implementation of change stream

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencyLocking { lockAllConfigurations() }
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
