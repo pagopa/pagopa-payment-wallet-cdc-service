2a4a687b688992a9397c52a2de6a982fcfcd7939
e.serrao <emanuele.serrao@bvtech.com>
2024-10-04 10:13:48 +0200

    fix: updated test imports

--- src/test/kotlin/it/pagopa/wallet/PagopaPaymentWalletCdcServiceApplicationTests.kt
+++ src/test/kotlin/it/pagopa/wallet/PagopaPaymentWalletCdcServiceApplicationTests.kt
@@ -1,7 +1,7 @@
 package it.pagopa.wallet
 
-import kotlin.test.Test
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
