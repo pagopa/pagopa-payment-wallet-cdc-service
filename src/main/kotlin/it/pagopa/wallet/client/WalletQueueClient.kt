0f308dccdc885258f4a7b950c3acf8785da5081f
e.serrao <emanuele.serrao@bvtech.com>
2024-10-08 17:12:50 +0200

    added retry mechanism

--- src/main/kotlin/it/pagopa/wallet/client/WalletQueueClient.kt
+++ src/main/kotlin/it/pagopa/wallet/client/WalletQueueClient.kt
@@ -8,8 +8,10 @@ import com.azure.storage.queue.models.SendMessageResult
 import it.pagopa.wallet.common.QueueEvent
 import it.pagopa.wallet.common.tracing.QueueTracingInfo
 import java.time.Duration
+import java.time.temporal.ChronoUnit
 import org.bson.BsonDocument
 import reactor.core.publisher.Mono
+import reactor.util.retry.Retry
 
 class WalletQueueClient(
     private val cdcQueueClient: QueueAsyncClient,
@@ -23,8 +25,12 @@ class WalletQueueClient(
         tracingInfo: QueueTracingInfo
     ): Mono<Response<SendMessageResult>> {
         val queueEvent = QueueEvent(event, tracingInfo)
-        return BinaryData.fromObjectAsync(queueEvent, jsonSerializer).flatMap {
-            cdcQueueClient.sendMessageWithResponse(it, delay, ttl)
-        }
+        return BinaryData.fromObjectAsync(queueEvent, jsonSerializer)
+            .flatMap { cdcQueueClient.sendMessageWithResponse(it, delay, ttl) }
+            .retryWhen(
+                Retry.backoff(3, Duration.of(1, ChronoUnit.SECONDS)).doBeforeRetry { signal ->
+                    println("Retrying due to: ${signal.failure().message}")
+                }
+            )
     }
 }
