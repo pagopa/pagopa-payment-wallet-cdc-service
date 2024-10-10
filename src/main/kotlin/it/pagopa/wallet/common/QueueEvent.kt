7df1eb9651cd4799d0c2c78e87f4cb79c9575305
CianoDanilo <danilo.ciano@pagopa.it>
2024-10-10 14:22:14 +0200

    chore(producer): refactor content type written to queue

--- src/main/kotlin/it/pagopa/wallet/common/QueueEvent.kt
+++ src/main/kotlin/it/pagopa/wallet/common/QueueEvent.kt
@@ -3,4 +3,4 @@ package it.pagopa.wallet.common
 import it.pagopa.wallet.common.tracing.QueueTracingInfo
 import org.bson.BsonDocument
 
-data class QueueEvent<T : BsonDocument>(val data: T, val tracingInfo: QueueTracingInfo)
+data class QueueEvent<T : String>(val data: T, val tracingInfo: QueueTracingInfo)
