bd404a125be35ace61045ee61607a90cd3a1edbc
CianoDanilo <danilo.ciano@pagopa.it>
2024-10-10 16:34:55 +0200

    wip(resume-policy): spotless apply

--- src/main/kotlin/it/pagopa/wallet/common/QueueEvent.kt
+++ src/main/kotlin/it/pagopa/wallet/common/QueueEvent.kt
@@ -1,6 +1,5 @@
 package it.pagopa.wallet.common
 
 import it.pagopa.wallet.common.tracing.QueueTracingInfo
-import org.bson.BsonDocument
 
 data class QueueEvent<T : String>(val data: T, val tracingInfo: QueueTracingInfo)
