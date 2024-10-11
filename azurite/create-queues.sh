969a807dd8b9df339dadcd3b0300ec531200027c
Simone Infante <simone.infante@pagopa.it>
2024-10-11 13:09:29 +0200

    wip: message check

--- azurite/create-queues.sh
+++ azurite/create-queues.sh
@@ -3,3 +3,4 @@
 az storage queue create -n pagopa-wallet-expiration-queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
 az storage queue create -n pagopa-wallet-cdc-queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
 
+timeout 20 bash -c "while [[ $(az storage message get -q pagopa-wallet-cdc-queue  --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1' | grep -c PAGOPA) -eq "0" ]]; do sleep 5; done" || exit 1
