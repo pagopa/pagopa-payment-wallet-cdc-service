3415b32ed12eca4f0a3d7ea159589a74b15f94f8
e.serrao <emanuele.serrao@bvtech.com>
2024-10-11 11:27:56 +0200

    update code

--- azurite/create-queues.sh
+++ azurite/create-queues.sh
@@ -6,5 +6,5 @@ for queue in $queues
     do
         queue_value=$(eval "echo \$$queue")
         echo "Found queue into env -> $queue_value, creating queue"
-        az storage queue create -n $queue_value --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://pagopa-wallet-azurite:10001/devstoreaccount1'
+        az storage queue create -n $queue_value --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
     done
