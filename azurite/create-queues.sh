8e08121aa8f7f4c70fb8a702a548d86a54980521
e.serrao <emanuele.serrao@bvtech.com>
2024-10-11 12:00:07 +0200

    fix script

--- azurite/create-queues.sh
+++ azurite/create-queues.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "Analizing env variables"
-queues=$(env | grep '^QUEUE_' | sed -n "s/^\(.*\)=.*/\1/p")
-echo "Found queues -> $queues,"
+az storage queue create -n pagopa-wallet-expiration-queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
+az storage queue create -n pagopa-wallet-cdc-queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
 
-for queue in $queues
-    do
-        queue_value=$(eval "echo \$$queue")
-        echo "Found queue into env -> $queue_value, creating queue"
-        az storage queue create -n $queue_value --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
-    done
