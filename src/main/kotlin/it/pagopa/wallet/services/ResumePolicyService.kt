f6fd4c5977a11dbbf8ec1e3e62a85b11fe3e3732
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-07-29 14:50:49 +0200

    chore(reactive-redis): Update redis configuration with reactive redis

--- src/main/kotlin/it/pagopa/wallet/services/ResumePolicyService.kt
+++ src/main/kotlin/it/pagopa/wallet/services/ResumePolicyService.kt
@@ -1,9 +1,10 @@
 package it.pagopa.wallet.services
 
+import reactor.core.publisher.Mono
 import java.time.Instant
 
 interface ResumePolicyService {
-    fun getResumeTimestamp(): Instant
+    fun getResumeTimestamp(): Mono<Instant>
 
-    fun saveResumeTimestamp(timestamp: Instant)
+    fun saveResumeTimestamp(timestamp: Instant): Mono<Boolean>
 }
