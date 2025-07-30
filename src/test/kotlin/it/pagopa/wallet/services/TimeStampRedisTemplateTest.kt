a66cec91f8ec6e3cef55514c8f753ef0e3dce206
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-07-30 14:37:32 +0200

    test(reactive-redis): Adapt test to reactive redis

--- src/test/kotlin/it/pagopa/wallet/services/TimeStampRedisTemplateTest.kt
+++ src/test/kotlin/it/pagopa/wallet/services/TimeStampRedisTemplateTest.kt
@@ -2,49 +2,62 @@ package it.pagopa.wallet.services
 
 import java.time.Duration
 import java.time.Instant
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.*
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.core.ValueOperations
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.core.ReactiveValueOperations
 import org.springframework.test.context.TestPropertySource
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
 
 @ExtendWith(MockitoExtension::class)
 @TestPropertySource(locations = ["classpath:application-test.properties"])
 class TimeStampRedisTemplateTest {
-    private val redisTemplate: RedisTemplate<String, Instant> = mock()
-    private val valueOps: ValueOperations<String, Instant> = mock()
+    private val reactiveRedisTemplate: ReactiveRedisTemplate<String, Instant> = mock()
+    private val valueOps: ReactiveValueOperations<String, Instant> = mock()
     private lateinit var timestampRedisTemplate: TimestampRedisTemplate
 
     @BeforeEach
     fun initTimeStampRedisTemplate() {
-        timestampRedisTemplate = TimestampRedisTemplate(redisTemplate)
+        timestampRedisTemplate = TimestampRedisTemplate(reactiveRedisTemplate)
     }
 
     @Test
     fun `time stamp redis template saves instant`() {
-        given { redisTemplate.opsForValue() }.willReturn(valueOps)
-        doNothing().`when`(valueOps).set(anyOrNull(), anyOrNull(), any<Duration>())
+        val expected = true
+        given { reactiveRedisTemplate.opsForValue() }.willReturn(valueOps)
+        given { valueOps.set(anyOrNull(), anyOrNull(), any<Duration>()) }
+            .willReturn(Mono.just(expected))
 
-        timestampRedisTemplate.save("keyspace", "target", Instant.now(), Duration.ofSeconds(0))
+        StepVerifier.create(
+                timestampRedisTemplate.save(
+                    "keyspace",
+                    "target",
+                    Instant.now(),
+                    Duration.ofSeconds(0)
+                )
+            )
+            .expectNext(expected)
+            .verifyComplete()
+
+        verify(reactiveRedisTemplate, times(1)).opsForValue()
         verify(valueOps, times(1)).set(anyOrNull(), anyOrNull(), any<Duration>())
-        verify(redisTemplate, times(1)).opsForValue()
     }
 
     @Test
     fun `time stamp redis template gets instant`() {
-        val now = Instant.now()
-        given { redisTemplate.opsForValue() }.willReturn(valueOps)
-        given { valueOps.get(anyOrNull()) }.willReturn(now)
+        val expected = Instant.now()
+        given { reactiveRedisTemplate.opsForValue() }.willReturn(valueOps)
+        given { valueOps.get(anyOrNull()) }.willReturn(Mono.just(expected))
 
-        val resumeTimestamp = timestampRedisTemplate.findByKeyspaceAndTarget("keyspace", "target")
+        StepVerifier.create(timestampRedisTemplate.findByKeyspaceAndTarget("keyspace", "target"))
+            .expectNext(expected)
+            .verifyComplete()
 
-        Assertions.assertTrue(!resumeTimestamp.isEmpty)
-        Assertions.assertTrue(resumeTimestamp.get() == now)
+        verify(reactiveRedisTemplate, times(1)).opsForValue()
         verify(valueOps, times(1)).get(anyOrNull())
-        verify(redisTemplate, times(1)).opsForValue()
     }
 }
